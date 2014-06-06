7f170e3c85d462a6edc07406f624c6b25e23bf5c
Grace F. Baumgardner <grace@cytobank.org>
2014-06-05 23:48:52 -0700

    Rebuilt the report winnowing of the raw data. Fixed how the raw data is generated in the first place. Added 'stale' functionality in the code for testing just the report winnowing/generation.

--- src/octypus.pl
+++ src/octypus.pl
@@ -20,22 +20,23 @@ my $cfg = LoadFile($cfh); # Load the configuration options into memory.
 
 print Dumper $cfg;
 
-my $test = $$cfg{'test'};
-my $server = $$cfg{'server'};
-my $iteration = $$cfg{'iterate'};
+my ($test, $server, $iteration) = ($$cfg{'test'}, $$cfg{'server'}, $$cfg{'iterate'});
 my ($s, $mi, $h, $d, $mo, $y, $wd, $yd, $dst);
 ( $s, $mi, $h, $d, $mo, $y, $wd, $yd, $dst ) = localtime(time);
-my $debug = 1;
+my $debug = 1;                                                          # Print debug messages.
+my $stale = 0;                                                          # Don't generate fresh raw stuff, just grab the raw file in $stale_octypus.
+
+my $stale_octypus = 'Raw_Octypus: Saturday_May-31-2014_at_19:36:25_ID_6c2bf31f-0709-3e8f-9e44-89d42eb75a7e.txt';
 
 my @final_analysis;
 
-if ($s < 10) { $s = "0" . $s };
+if ($s < 10) { $s = ("0" . $s) };
 if ($mi < 10) { $mi = "0" . $mi };
 
 my @days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 my @months = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
-my $date = $days[$wd] . "_" . $months[$mo] . "-" . ($d+1) . "-" . ($y+1900);
+my $date = $days[$wd] . "_" . $months[$mo] . "-" . ($d) . "-" . ($y+1900);
 my $time = $h . ":" . $mi . ":" . $s;
 
 my $run_uuid = create_uuid_as_string(UUID_MD5, "$test$server$time$date");
@@ -43,6 +44,7 @@ my $run_uuid = create_uuid_as_string(UUID_MD5, "$test$server$time$date");
 my $current_raw_file = 'Raw_Octypus: ' . $date . "_at_" . $time . "_ID_" . $run_uuid . ".txt";
 my $current_report_file = 'Cooked_Octypus: ' . $date . "_at_" . $time . "_ID_" . $run_uuid . ".txt";
 
+if ($debug) { print Dumper @final_analysis; }
 
 # CONFIGURATION OF PLATFORMS TO TEST ON
 
@@ -50,8 +52,16 @@ open(my $pfh, '<', '../config/platforms.yaml') or die "Can't load platform list.
 
 my $platforms = LoadFile($pfh); # Load the platforms file into memory.
 
-print Dumper $platforms;
+if ($debug) { print Dumper $platforms; }
 
+
+if ( $stale ) {                                 # If we're working with stale Octypus, then we ignore 'new' file info and just fetch a specific raw file.
+
+        $current_raw_file = $stale_octypus;
+
+}
+
+if ( $stale ) { goto STALE; }
 
 # OPEN UP THE RAW FILEHANDLE
 
@@ -61,32 +71,29 @@ my $test_and_platform;
 
 foreach (@{$platforms}) {
 
-	$test_and_platform = "$test using $$_[0] v. $$_[2] on $$_[1]";			# Stash this to standardize and save typing complex refs.
+        $test_and_platform = "$test using $$_[0] v. $$_[2] on $$_[1]";                  # Stash this to standardize and save typing complex refs
 
-	if ($debug) { print "Just got into the platforms loop."; }
+        if ($debug) { print "Just got into the platforms loop."; }
 
-	($s, $mi, $h, $d, $mo, $y, $wd, $yd, $dst) = localtime(time);
-	my $when = "$mo-$d-" . ($y + 1900) . " at $h:";
-	if ($s < 10) { $when = $when . "$s"; } else { $when = $when . "0" . $s; }
+        ($s, $mi, $h, $d, $mo, $y, $wd, $yd, $dst) = localtime(time);
+        my $when = "$mo-$d-" . ($y + 1900) . " at $h:";
+        if ($s < 10) { $when = $when . "$s"; } else { $when = $when . "0" . $s; }
 
-	print "\n\n", qq(  >>> BEGIN RESULTS FOR $test_and_platform on $when <<< ), "\n\n";	# Show in output.
-	print $rfh "\n\n", qq(  >>> BEGIN RESULTS FOR $test_and_platform on $when <<< ), "\n\n";	# Add to the raw result file.
+        my $i = 0;
 
-	my $i = 0;
+        while ($i < $iteration) {
 
-	while ($i < $iteration) {
+                $i++;
+                print "\n\n", qq(  >>> BEGIN RESULT $i FOR $test_and_platform on $when <<< ), "\n\n";           # Show in output.
+                print $rfh "\n\n", qq(  >>> BEGIN RESULT $i FOR $test_and_platform on $when <<< ), "\n\n";      # Add to the raw result file.
+                my $output = `/usr/local/bin/gless $test $server GE_BROWSER='$$_[0]' GE_PLATFORM='$$_[1]' GE_BROWSER_VERSION='$$_[2]'`;
+                print $output;
+                print $rfh $output;
+                print "\n\n", qq(  >>> END RESULTS FOR $test_and_platform started on $when <<< ), "\n\n";       # Show in output.
+                print $rfh "\n\n", qq(  >>> END RESULTS FOR $test_and_platform started on $when <<< ), "\n\n";  # Add to the raw result file.
 
-		my $output = `/usr/local/bin/gless $test $server GE_BROWSER='$$_[0]' GE_PLATFORM='$$_[1]' GE_BROWSER_VERSION='$$_[2]'`;
-		$i++;
-		print $output;
-		print $rfh $output;
+        }
 
-	}
-
-	# print $output; # change this to dump to a textfile after stripping out warnings, for ease of reading afterwards
-
-        print "\n\n", qq(  >>> END RESULTS FOR $test_and_platform started on $when <<< ), "\n\n";	# Show in output.
-	print $rfh "\n\n", qq(  >>> END RESULTS FOR $test_and_platform started on $when <<< ), "\n\n";	# Add to the raw result file. Refactor these?
 
 }
 
@@ -96,76 +103,83 @@ close($rfh);
 
 # PROCESS RAW DATA INTO REPORT DATA
 
-open($rfh, '<', ('../output/raw/' . $current_raw_file)) || die "Can't read raw file $current_raw_file for processing.";
+STALE: open($rfh, '<', ('../output/raw/' . $current_raw_file)) || die "Can't read raw file $current_raw_file for processing.";
 
-my @reportlines;			# The array for the lines we need for the report.
-my @rawlines = <$rfh>;			# The array with all the raw lines from the output.
-my $numlines = scalar(@rawlines);	# Get the number of lines so we can iterate but still pick lines.
-my $footer = 0;				# The flag for whether we're in the footer.
+open (my $rfh, '<', ('../output/raw/' . $current_raw_file)) || die "Can't read raw file $current_raw_file for processing.";
 
-# print "We're cooking with gas now.\n\n";
+my @reportlines;                                        # This is where the report lines will end up.
+my @raw = <$rfh>;                                       # Stash the lines.
+my $count = scalar(@raw);                               # Count the lines.
+my $block = 0;                                          # Until we hear otherwise, do not assume we're in a test block.
+my $catch = 0;
+my $footer = 0;
+my @states;
 
-# print Dumper @rawlines;
+close( $rfh );
 
-my $state = "PASSED";			# Until we hear otherwise, assume the tests pass.
+for ( my $current = 0; $current < $count; $current++) {
 
-for ( my $linenum = 0; $linenum < $numlines; $linenum++) {
+        $catch = 0;
 
-#	print "We began an iteration on line number $linenum of $current_raw_file.\n";
-	my $line = $rawlines[$linenum];							# The line we're on.
-	my $prevline; if ($linenum > 0) { $prevline = $rawlines[$linenum - 1]; }	# The line before this one, if there is one.
-	my $caught = 0;									# Whether we've caught this line once. (We won't need 'em twice.)
-	chomp ($line);
-	if ($linenum > 0) { chomp ($prevline); }
-	
-	if ($line =~ /^  >>> BEGIN/ && !$caught) {
-		push @reportlines, "$line\n\n";
-		$caught = 1;
-		if ($debug) { print "Beginning the report.\n"; }			# Add the results header line
-	}
+        my $line = $raw[$current];
+        my $prevline;
+        my $nextline;
+        if ($current > 0) { $prevline = $raw[$current - 1]; }
+        $nextline = $raw[$current + 1];
+        chomp($line);
 
-	if ($line =~ /^( ){6}/ && !$caught) {						# If a Ruby fail output block has started...
-		if ($debug) { print "Detected an error indentation.\n"; }
-		unless ($prevline =~ /^( ){6}/) {					# ...and the line before it isn't part of the output block...
-			if ($debug) { print "Detected error line just previous.\n"; }
-			push @reportlines, "$prevline \n\n";				# ...add it to the report so we know what failed.
-			$caught = 1;
-			if ($debug) { print "Caught an error line.\n"; }
-		}
-	}
 
-	if ($line =~ /^Failing/ && !$caught) {
-		push @reportlines, "$line\n";
-		$footer = 1;
-		$caught = 1;
-		$state = "FAILED";
-		if ($debug) { print "Caught the start of the footer.\n"; } 		# Add the 'Failing' line and signal the start of the footer
-	}
+        if ($line =~ /^  >>> END/) {
 
-	if ($line =~ /^[0-9]* scenario/ && !$caught) {
-		push @reportlines, "$line\n";
-		$caught = 1;
-		$footer = 1;
-		if ($debug) { print "Caught a scenario count line.\n"; }		# Mark footer started if the scenario line shows up.
-	}
+                $footer = 0;                                                    # End the footer if it's going.
+                $catch = 1;                                                     # Capture the line.
+                $block = 0;                                                     # Turn off this test block.
 
-	if ($footer && !$caught) {
-		push @reportlines, "$line \n";
-		$caught = 1;
-		if ($debug) { print "Caught a footer line.\n"; }			# Add any line we see while we're in the footer.
-	}
+        }
 
-	if ($line =~ /^  >>> END/ && !$caught) { 
-		push @reportlines, "$line\n"; 						# Add the last line.
-		push @reportlines, "           >>>>> $test_and_platform: $state <<<<<\n";
-		push @final_analysis, "           >>>>> $test_and_platform: $state <<<<<\n";
-		$caught = 1;
-		$footer = 0;								# The footer's all finished now.
-		if ($debug) { print "Caught the end of the footer.\n"; }
-	}
+        if ($line =~ /^  >>> BEGIN/) {
+
+                $catch = 1;                                                     # Capture the line.
+                $block = 1;                                                     # Turn on a new test block.
+
+        }
+
+
+
+        if ($block) {                                                           # These are things we only look for in the block.
+
+                if ($footer) { $catch = 1; }
+
+
+                if (($line =~ /^( ){6}[A-Za-z]+?/) && ($prevline =~ /^( ){4}/)) {
+
+                        $catch = 1;                                             # Capture the first indented line for errors.
+
+                }
+
+
+                if (($line =~ /^( ){4}[A-Za-z]+?/) && ($nextline =~ /^( ){6}/)) {
+
+                        $catch = 1;                                             # Capture the line just before indents for errors.
+
+                }
+
+
+                if (($line =~ /^Failing/) || ($line =~ /^[0-9]{1,4}/)) {
+
+                        $footer = 1;
+                        if ($line =~ /^Failing/) { push @states, "FAILED"; }
+
+                }
+
+        }
+
+        print "On iteration $current, just before the catch check: catch = $catch, block = $block, footer = $footer\n";
+        print "Line: $line\n\n";
+
+        if ($catch) { push @reportlines, $line . "\n"; }
 
 }
-
 
 print "Report finished. Saving.\n\n";
 
@@ -174,9 +188,9 @@ print "Report finished. Saving.\n\n";
 
 open(my $repfh, '>', ('../output/reports/' . $current_report_file)) || die "Couldn't open $current_report_file to save sanitized results. \n";
 
-foreach (@reportlines) { 
+foreach (@reportlines) {
 
-	print $repfh $_;
+        print $repfh $_;
 
 }
 
@@ -186,15 +200,23 @@ print $repfh "  >> REPORT SUMMARY FOR $run_uuid on $date at $time << \n";
 print $repfh "  ************************************************************************************************************************ \n";
 
 my $number_of_results = scalar(@final_analysis);
-my $difference = $iteration - $number_of_results;
+if ($debug) { print ' *** DUMPER RESULTS FOR @final_analysis *** ', "\n"; print Dumper @final_analysis; }
+if ($debug) { print Dumper $number_of_results; }
+
+my $difference = $iteration - ($number_of_results + 1);
+
+if ($debug) { print Dumper $difference; }
+
 my $comparison;
+
+if ($debug) { print Dumper $comparison; }
 
 if ($difference > 0) { $comparison = "fewer"; }
 if ($difference < 0) { $comparison = "more"; }
 
 if ($comparison) {
-	print $repfh "  We were supposed to do $iteration but did $difference $comparison tests than indicated.\n";
-	print $repfh "  >>There may be a problem with the underlying Ruby executions.<<\n\n";
+        print $repfh "  We were supposed to do $iteration but did $difference $comparison tests than indicated.\n";
+        print $repfh "  >>There may be a problem with the underlying Ruby executions.<<\n\n";
 }
 
 if ($difference == 0) { print $repfh "  We did exactly the number of indicated tests: $number_of_results \n\n"; }
@@ -202,7 +224,7 @@ if ($difference == 0) { print $repfh "  We did exactly the number of indicated t
 
 foreach (@final_analysis) {
 
-	print $repfh $_;
+        print $repfh $_;
 
 }
 
@@ -210,4 +232,3 @@ close($repfh);
 
 print "Octypus Report saved as $current_report_file. Thank you for using Octypus.\n\n";
 
-__END__
