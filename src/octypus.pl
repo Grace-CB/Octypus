92e4ffdead15cd1a923eb1d4c2c71a9eb7a53437
Grace F. Baumgardner <grace@cytobank.org>
2014-06-12 21:29:34 -0700

    Fixed summary counts and report issues.

--- src/octypus.pl
+++ src/octypus.pl
@@ -11,6 +11,8 @@ use UUID::Tiny ':std';
 use YAML::Any qw( Dump LoadFile ); #load up whatever YAML we're generally using
 use Data::Dumper;
 
+my $debug = 0;                                                          # Print debug messages.
+
 
 # GENERAL CONFIGURATION
 
@@ -18,17 +20,14 @@ open(my $cfh, '< ', "../config/config.yaml") or die "Can't load config options.\
 
 my $cfg = LoadFile($cfh); # Load the configuration options into memory.
 
-print Dumper $cfg;
+if ($debug) { print Dumper $cfg; }
 
 my ($test, $server, $iteration) = ($$cfg{'test'}, $$cfg{'server'}, $$cfg{'iterate'});
 my ($s, $mi, $h, $d, $mo, $y, $wd, $yd, $dst);
 ( $s, $mi, $h, $d, $mo, $y, $wd, $yd, $dst ) = localtime(time);
-my $debug = 1;                                                          # Print debug messages.
-my $stale = 0;                                                          # Don't generate fresh raw stuff, just grab the raw file in $stale_octypus.
+my $stale = 1;                                                          # Don't generate fresh raw stuff, just grab the raw file in $stale_octypus.
 
-my $stale_octypus = 'Raw_Octypus: Saturday_May-31-2014_at_19:36:25_ID_6c2bf31f-0709-3e8f-9e44-89d42eb75a7e.txt';
-
-my @final_analysis;
+my $stale_octypus = 'Raw_Octypus: Thursday_June-12-2014_at_17:34:29_ID_df5b38c8-39af-3dfe-91b0-edae44e998e8.txt';
 
 if ($s < 10) { $s = ("0" . $s) };
 if ($mi < 10) { $mi = "0" . $mi };
@@ -44,7 +43,6 @@ my $run_uuid = create_uuid_as_string(UUID_MD5, "$test$server$time$date");
 my $current_raw_file = 'Raw_Octypus: ' . $date . "_at_" . $time . "_ID_" . $run_uuid . ".txt";
 my $current_report_file = 'Cooked_Octypus: ' . $date . "_at_" . $time . "_ID_" . $run_uuid . ".txt";
 
-if ($debug) { print Dumper @final_analysis; }
 
 # CONFIGURATION OF PLATFORMS TO TEST ON
 
@@ -61,7 +59,7 @@ if ( $stale ) {                                 # If we're working with stale Oc
 
 }
 
-if ( $stale ) { goto STALE; }
+if ( $stale ) { goto STALE; }  			# NOT THE DREADED GOTO STATEMENT okay yes I used a goto statement in Perl
 
 # OPEN UP THE RAW FILEHANDLE
 
@@ -104,8 +102,6 @@ close($rfh);
 # PROCESS RAW DATA INTO REPORT DATA
 
 STALE: open($rfh, '<', ('../output/raw/' . $current_raw_file)) || die "Can't read raw file $current_raw_file for processing.";
-
-open (my $rfh, '<', ('../output/raw/' . $current_raw_file)) || die "Can't read raw file $current_raw_file for processing.";
 
 my @reportlines;                                        # This is where the report lines will end up.
 my @raw = <$rfh>;                                       # Stash the lines.
@@ -167,19 +163,23 @@ for ( my $current = 0; $current < $count; $current++) {
 
                 if (($line =~ /^Failing/) || ($line =~ /^[0-9]{1,4}/)) {
 
+			$catch = 1;
                         $footer = 1;
                         if ($line =~ /^Failing/) { push @states, "FAILED"; }
+			print Dumper @states;
 
                 }
 
         }
 
-        print "On iteration $current, just before the catch check: catch = $catch, block = $block, footer = $footer\n";
-        print "Line: $line\n\n";
+
+        if ($debug) { print "On iteration $current, just before the catch check: catch = $catch, block = $block, footer = $footer\n"; }
+        if ($debug) { print "Line: $line\n\n"; }
 
         if ($catch) { push @reportlines, $line . "\n"; }
 
 }
+
 
 print "Report finished. Saving.\n\n";
 
@@ -196,14 +196,19 @@ foreach (@reportlines) {
 
 print $repfh "\n";
 print $repfh "  ************************************************************************************************************************ \n";
-print $repfh "  >> REPORT SUMMARY FOR $run_uuid on $date at $time << \n";
+print $repfh "  >>";
+if ($stale) { print $repfh "STALE RUN"; }
+print $repfh "REPORT SUMMARY FOR $run_uuid on $date at $time << \n";
 print $repfh "  ************************************************************************************************************************ \n";
 
-my $number_of_results = scalar(@final_analysis);
-if ($debug) { print ' *** DUMPER RESULTS FOR @final_analysis *** ', "\n"; print Dumper @final_analysis; }
+
+my $number_of_results = scalar(@states);
+if ($debug) { print ' *** DUMPER RESULTS FOR @final_analysis *** ', "\n"; print Dumper @states; }
 if ($debug) { print Dumper $number_of_results; }
 
-my $difference = $iteration - ($number_of_results + 1);
+print "Okay, number of results = $number_of_results, and iteration count = $iteration.";
+
+my $difference = $iteration - $number_of_results;
 
 if ($debug) { print Dumper $difference; }
 
@@ -215,20 +220,33 @@ if ($difference > 0) { $comparison = "fewer"; }
 if ($difference < 0) { $comparison = "more"; }
 
 if ($comparison) {
+	print "$iteration tests scheduled but $difference $comparison tests done. \n\n";
         print $repfh "  We were supposed to do $iteration but did $difference $comparison tests than indicated.\n";
         print $repfh "  >>There may be a problem with the underlying Ruby executions.<<\n\n";
 }
 
-if ($difference == 0) { print $repfh "  We did exactly the number of indicated tests: $number_of_results \n\n"; }
+if ($difference == 0) {
+
+	print "  We did exactly the number of indicated tests: $number_of_results \n\n";
+	print $repfh "  We did exactly the number of indicated tests: $number_of_results \n\n";
+
+}
 
 
-foreach (@final_analysis) {
+print "Generating final summary.\n";
+print $repfh " ** FINAL SUMMARY ** \n";
 
-        print $repfh $_;
+my $i = 0;
+
+foreach (@states) {
+
+	$i++;
+        print $repfh "Iteration $i $_ \n";
 
 }
 
 close($repfh);
 
-print "Octypus Report saved as $current_report_file. Thank you for using Octypus.\n\n";
+if ($stale) { print " ** STALE RUN ** \n"; }
 
+print "Octypus Report saved as $current_report_file. Thank you for using Octypus.\n\n";
